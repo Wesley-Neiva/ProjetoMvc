7de3cf165063923005f3494180c1f884932545bc
Wesley <yelsew84@gmail.com>
2023-01-31 22:35:26 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SallesRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SallesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SallesRecord() { }
+
+        public SallesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
