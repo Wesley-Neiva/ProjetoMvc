b3c8638754fb5e72d273d92a2cbd36a0f33f945c
Wesley <yelsew84@gmail.com>
2023-02-04 10:11:56 -0300

    Foreign key not null(referential integrity

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -11,6 +11,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SallesRecord> Sales { get; set; } = new List<SallesRecord>();
 
         public Seller() { }
